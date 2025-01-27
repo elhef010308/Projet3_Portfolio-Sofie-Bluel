7bfc73d385cc23f8c3a38a79a980bd07187e0abc
Elhef_2038 <lfougerard@gmail.com>
2025-01-27 10:22:57 +0100

    Réalisation de l'étape 1.1 du projet 3

---
+++ FrontEnd/script.js
@@ -0,0 +1,51 @@
+const galleryContainer = document.querySelector(".gallery");
+let galleryItems= [];
+
+async function fetchWorks() {
+    const reponse = await fetch("http://localhost:5678/api/works");
+    galleryItems = await reponse.json();
+    console.log(galleryItems);
+
+    galleryContainer.innerHTML = '';
+    console.log(galleryContainer);
+} 
+
+async function addElement() {
+    for (let i=0; i<galleryItems.length; i++) {
+        const figureContainer = document.createElement("figure");
+
+        const baliseImage = document.createElement("img");
+        baliseImage.setAttribute("alt", galleryItems[i].title);
+        baliseImage.setAttribute("src", galleryItems[i].imageUrl);
+
+        const baliseFigcaption = document.createElement("figcaption");
+        baliseFigcaption.textContent = galleryItems[i].title;
+
+        figureContainer.appendChild(baliseImage);
+        figureContainer.appendChild(baliseFigcaption);
+
+        galleryContainer.appendChild(figureContainer);
+    }  
+
+    console.log(galleryContainer);
+}
+
+ /* 
+    Attention il faut appeler addElement après fetchWorks :
+        1- on appelle fetchWorks
+        2- ".then()" = une fois la promesse terminée alors...
+        3- ...alors on appelle les fonctions addElement et newFilters 
+*/
+fetchWorks().then(() => {
+    addElement();
+});
+
+
+
+
+
+
+
+
+
+
